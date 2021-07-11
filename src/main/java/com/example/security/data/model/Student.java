5da6143a94d0431a563c89f6062e741ac05a93ec
whalewalker <mongodbms@gmail.com>
2021-07-11 19:24:39 +0100

    create student api without security

---
+++ src/main/java/com/example/security/data/model/Student.java
@@ -0,0 +1,15 @@
+package com.example.security.data.model;
+
+import lombok.Data;
+
+@Data
+public class Student {
+    private final Integer id;
+    private final String name;
+
+    public Student(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+}
