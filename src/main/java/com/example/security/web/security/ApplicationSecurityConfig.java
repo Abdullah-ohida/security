a7104993a38420dc5b38d60a19551d2ee14a34b2
whalewalker <mongodbms@gmail.com>
2021-07-12 14:02:28 +0100

    added user to in memory user details manager

--- src/main/java/com/example/security/web/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/security/web/security/ApplicationSecurityConfig.java
@@ -1,9 +1,14 @@
 package com.example.security.web.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
@@ -18,5 +23,16 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authenticated()
                 .and()
                 .httpBasic();
+    }
+
+    @Override
+    @Bean
+    public UserDetailsService userDetailsServiceBean() throws Exception {
+        UserDetails annaSmithUser = User.builder()
+                .username("anna smith")
+                .password("password")
+                .roles("STUDENT").build();
+
+        return new InMemoryUserDetailsManager(annaSmithUser);
     }
 }
